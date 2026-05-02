e142733cdd00214d3bee8626babd49913cea3408
shameesmanikuni <53367079+shameesmanikuni@users.noreply.github.com>
2026-05-02 13:49:09 +0530

    Working version of Side Pane

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,11 +1,20 @@
 using Microsoft.UI;
 using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media;
+using Windows.UI;
 
 namespace EtherealPDF
 {
     public sealed partial class MainWindow : Window
     {
+        private readonly SolidColorBrush _activeBgBrush = new SolidColorBrush(Color.FromArgb(51, 81, 39, 173));  // #335127AD (20% Opacity)
+        private readonly SolidColorBrush _activeFgBrush = new SolidColorBrush(Color.FromArgb(255, 116, 209, 255)); // #74D1FF
+
+        private readonly SolidColorBrush _inactiveBgBrush = new SolidColorBrush(Color.FromArgb(0, 0, 0, 0));       // Transparent
+        private readonly SolidColorBrush _inactiveFgBrush = new SolidColorBrush(Color.FromArgb(255, 190, 200, 207)); // #BEC8CF
+
         public MainWindow()
         {
             this.InitializeComponent();
@@ -46,6 +55,39 @@ namespace EtherealPDF
             // Inactive states (when the app loses focus)
             titleBar.ButtonInactiveBackgroundColor = surfaceColor;
             titleBar.ButtonInactiveForegroundColor = Windows.UI.Color.FromArgb(255, 120, 120, 120);
+        }
+
+        private void NavButton_Click(object sender, RoutedEventArgs e)
+        {
+            ResetButtonStyles();
+
+            if(sender is Button clickedBtn)
+            {
+                clickedBtn.Background = _activeBgBrush;
+                clickedBtn.Foreground = _activeFgBrush;
+
+                switch(clickedBtn.Name)
+                {
+                    case "LibraryBtn":
+                        break;
+                    case "ReadingBtn":
+                        break;
+                    case "RecentsBtn":
+                        break;
+                    case "SettingsBtn":
+                        break;
+                }
+            }
+        }
+
+        private void ResetButtonStyles()
+        {
+            Button[] navButtons = { LibraryBtn, ReadingBtn, RecentsBtn, SettingsBtn };
+            foreach(var btn in navButtons)
+            {
+                btn.Foreground = _inactiveFgBrush;
+                btn.Background = _inactiveBgBrush;
+            }
         }
     }
 }
