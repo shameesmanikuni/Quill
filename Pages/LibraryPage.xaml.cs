510254f86dc27b59ec155015a2724c1bdcb8cbe0
shameesmanikuni <53367079+shameesmanikuni@users.noreply.github.com>
2026-05-08 04:53:24 +0530

    Fixed minor bugs of background, And 'Continue Reading' cards

--- Pages/LibraryPage.xaml.cs
+++ Pages/LibraryPage.xaml.cs
@@ -1,20 +1,21 @@
+using Microsoft.UI;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Media.Animation;
+using Microsoft.UI.Xaml.Navigation;
+using Quill.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Xml.Linq;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
-using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Quill.ViewModels;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using Microsoft.UI.Xaml.Media.Animation;
-using Microsoft.UI;
 
 namespace Quill.Pages
 {
@@ -363,3 +364,15 @@ namespace Quill.Pages
         }
     }
 }
+
+
+
+//Check this out.
+//< Border x: Name = "CardBackground" CornerRadius = "16" >
+//                        < Border.Background >
+//                            < AcrylicBrush TintColor = "#1B1B1C"
+//                                          TintOpacity = "0.6"
+//                                          TintLuminosityOpacity = "0.4"
+//                                          FallbackColor = "#1B1B1C" />
+//                        </ Border.Background >
+//                    </ Border >
